010c738e52e1dfa4a2486fcbb704499980a997a4
Marianna Gokova <gokova.marina17@gmail.com>
2025-06-12 17:02:01 +0000

    Unified transactions from location_1, location_2, and location_3 into one model. Added monthly aggregation of ticket sales and revenue by movie and location.

---
+++ models/silver_screen/union_transactions.sql
@@ -0,0 +1,41 @@
+{{ config(materialized='table') }}
+
+WITH unioned AS (
+    SELECT
+        MOVIE_ID AS movie_id,
+        'location_1' AS location,
+        DATE_TRUNC('month', TIMESTAMP) AS month,
+        TICKET_AMOUNT AS ticket_quantity,
+        PRICE AS price
+    FROM {{ source('silver_screen', 'transactions_l1') }}
+
+    UNION ALL
+
+    SELECT
+        MOVIE_ID AS movie_id,
+        'location_2' AS location,
+        DATE_TRUNC('month', DATE) AS month,
+        TICKET_AMOUNT AS ticket_quantity,
+        TICKET_PRICE AS price
+    FROM {{ source('silver_screen', 'transactions_l2') }}
+
+    UNION ALL
+
+    SELECT
+        DETAILS AS movie_id,
+        'location_3' AS location,
+        DATE_TRUNC('month', TIMESTAMP) AS month,
+        AMOUNT AS ticket_quantity,
+        PRICE AS price
+    FROM {{ source('silver_screen', 'transactions_l3') }}
+    WHERE PRODUCT_TYPE = 'ticket'
+)
+
+SELECT
+    movie_id,
+    location,
+    month,
+    SUM(ticket_quantity) AS total_tickets_sold,
+    SUM(ticket_quantity * price) AS total_revenue
+FROM unioned
+GROUP BY movie_id, location, month
